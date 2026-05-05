96f1df5e0f215aa1e850bb8af28c9c4b91ca5990
Melody Laffitau <melody.laffitau@gmail.com>
2026-05-05 17:29:49 +0200

    More testing - done for today

--- src/socket/SocketServer.cpp
+++ src/socket/SocketServer.cpp
@@ -70,19 +70,19 @@ void    SocketServer::createSocket(){
 		throw SocketException("Error : listen has failed");
 }
 
-	static bool setSocket(int sockFd){
-		int opt = 1;
-		if (setsockopt(sockFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
-			return false;
+static bool setSocket(int sockFd){
+	int opt = 1;
+	if (setsockopt(sockFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+		return false;
 
-		int flags = fcntl(sockFd, F_GETFL, 0);
-		if (flags < 0)
-			return false;
-		if (fcntl(sockFd, F_SETFL, flags | O_NONBLOCK) < 0)
-			return false;
+	int flags = fcntl(sockFd, F_GETFL, 0);
+	if (flags < 0)
+		return false;
+	if (fcntl(sockFd, F_SETFL, flags | O_NONBLOCK) < 0)
+		return false;
 
-		return true;
-	}
+	return true;
+}
 
 int SocketServer::initStructGetaddrinfo(struct addrinfo& hints, struct addrinfo** res){
 	std::memset(&hints, 0, sizeof(hints));
