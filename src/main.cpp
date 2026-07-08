c0a9c70efe50460475dc6002caa231eb6c1f65a2
Hygie Lichtenthaeler <hlichten@c3r6s5.42lausanne.ch>
2026-07-08 17:19:44 +0200

    suite

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,6 @@
 #include <iostream>
 #include <csignal>
 
-//WebServer* gSignal = NULL;
-
-//void	testPrintParse();
-
 static WebServer* g_server = 0;
 
 void handle_sigint(int signum) {
@@ -61,73 +57,3 @@ int main (int ac, char **av)
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// print les element de la classe pour voir si tt ok
-void	testPrintParse()
-{
-	std::cout << "Test print verif" << std::endl;
-	ParserConfig parser;
-	parser.parse("config/4-everything.conf");
-	const std::vector<ServerConfig> &servers = parser.getServers();
-	for (size_t i = 0; i < servers.size(); i++) {
-		std::cout << "Server " << i << ":" << std::endl;
-		std::cout << "  Port: " << servers[i].port << std::endl
-				  << "  Host: " << servers[i].host << std::endl
-				  << "  Server Name: " << servers[i].serverName << std::endl
-				  << "  Max Body Size: " << servers[i].maxBodySize << std::endl
-				  << "  Root: " << servers[i].root << std::endl;
-		std::cout << "  Index: ";
-        for (size_t idx = 0; idx < servers[i].index.size(); idx++)
-            std::cout << servers[i].index[idx] << " ";
-        std::cout << std::endl;
-		std::cout << "  Error Pages:" << std::endl;
-		std::map<int, std::string>::const_iterator itErr;
-		for (itErr = servers[i].errorPages.begin(); itErr != servers[i].errorPages.end(); ++itErr) {
-			std::cout << "    Code " << itErr->first << " -> " << itErr->second << std::endl;
-		}
-		// contuinuer avec location apres 
-		std::cout << "  Locations:" << std::endl;
-		for (size_t j = 0; j < servers[i].locations.size(); j++) {
-			std::cout << " Location " << j << ":" << std::endl;
-			std::cout << "   Path: " << servers[i].locations[j].path << std::endl
-					  << "   Root: " << servers[i].locations[j].root << std::endl
-					  << "   Autoindex: " << (servers[i].locations[j].autoindex ? "on" : "off") << std::endl;
-			std::cout << "   Index: ";
-            for (size_t idx = 0; idx < servers[i].locations[j].index.size(); idx++)
-                std::cout << servers[i].locations[j].index[idx] << " ";
-            std::cout << std::endl
-					  << "   Max Body Size: " << servers[i].locations[j].maxBodySize << std::endl
-					  << "   Return URL: " << servers[i].locations[j].returnUrl << std::endl
-					  << "   Upload Store: " << servers[i].locations[j].uploadStore << std::endl
-					  << "   Methods: ";
-			for (size_t k = 0; k < servers[i].locations[j].methods.size(); k++) {
-				std::cout << servers[i].locations[j].methods[k] << " ";
-			}
-			std::cout << std::endl
-					  << "   CGI Enabled: " << (servers[i].locations[j].cgiEnabled ? "yes" : "no") << std::endl;
-			if (servers[i].locations[j].cgiEnabled) {
-				std::cout << "      CGI Handlers:" << std::endl;
-				std::map<std::string, std::string>::const_iterator itCgi;
-				for (itCgi = servers[i].locations[j].cgiInfo.begin(); itCgi != servers[i].locations[j].cgiInfo.end(); ++itCgi) {
-					std::cout << "        Extension: " << itCgi->first << " -> Path: " << itCgi->second << std::endl;
-				}
-			}
-			std::cout << std::endl;
-		}
-	}
-}
